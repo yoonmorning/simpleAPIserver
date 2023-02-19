574e2c61c6f20b6d948e9ac8d4373eee7cbfd2af
subin <ysb910405@naver.com>
2023-02-20 03:38:18 +0900

    [ADD] 유저 회원가입 엔드포인트 구현

---
+++ app.js
@@ -0,0 +1,72 @@
+const users = [
+  {
+    id: 1,
+    name: "Rebekah Johnson",
+    email: "Glover12345@gmail.com",
+    password: "123qwe",
+  },
+  {
+    id: 2,
+    name: "Fabia Predovic",
+    email: "Conne1129@gmail.com",
+    password: "password",
+  },
+];
+const posts = [
+  {
+    id: 1,
+    title: "간단한 HTTP API 개발 시작!",
+    content: "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server",
+    userId: 1,
+  },
+  {
+    id: 2,
+    title: "HTTP의 특성",
+    content: "Request/Response와 Stateless!",
+    userId: 2,
+  },
+];
+
+const http = require("http");
+
+const server = http.createServer();
+
+const httpRequestListener = function (request, response) {
+  const { url, method } = request;
+
+  if (method === "GET") {
+    if (url === "/ping") {
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.end(JSON.stringify({ message: "pong" }));
+    }
+  } else if (method === "POST") {
+    if (url === "/users/signup") {
+      let body = "";
+
+      request.on("data", (data) => {
+        body += data;
+      });
+
+      request.on("end", () => {
+        const user = JSON.parse(body);
+
+        users.push({
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          password: user.password,
+        });
+      });
+      response.end(JSON.stringify({ message: "userCreated" }));
+    }
+  }
+};
+
+server.on("request", httpRequestListener);
+
+const IP = "127.0.0.1";
+const PORT = 8000;
+
+server.listen(PORT, IP, function () {
+  console.log(`listening to request on ip ${IP} & ${PORT}`);
+});
